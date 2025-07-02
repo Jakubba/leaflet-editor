c79acf8ff382627ea416198ee0b3cd5dc6c6efc2
Jakub Barszcz <jakub.barszcz33@gmail.com>
2025-07-02 23:19:25 +0200

    fix: delete last not nesessery import react

--- src/components/ExportToPDF/ExportToPDF.tsx
+++ src/components/ExportToPDF/ExportToPDF.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import useStore from '../../store';
 import { Button } from '@mui/material';
 import PDFPage from './PDFPage';
